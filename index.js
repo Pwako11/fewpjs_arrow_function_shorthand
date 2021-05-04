741755b0b4972794fe2621b0e9748237e6ae5d94
Pwako11 <akolo@hotmail.com>
2021-05-04 07:03:46 -0400

    commit tree 2d409bfeb5467b7ec1edae0011f9731fbf59f6be

---
+++ index.js
@@ -0,0 +1,17 @@
+const num1 = 2000
+const num2 = 100
+
+console.log(num1)
+console.log(num2)
+
+const divide = function() {
+    let result = (2000/100)
+    
+    return result; 
+}
+
+divide()
+
+const square = (num) => num*num
+
+const add = (num1, num2) => num1 + num2
